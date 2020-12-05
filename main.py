382c4ecc77aa85f98557970849e4dd13dfa897ae
Mihir Patel <mihir20011patel@gmail.com>
2020-12-05 18:15:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
+import numpy as np
+
+
 pygame.init()
 
 win = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption("SuDOku")
 
+arr = np.zeros((9,9), dtype=int)  ## Makng array of 9X9 to store the values
+arr[1,3] = 8
+pos = [0, 0]
+
 ## DROW IN GAME WIN
 def win_dr(win):
-    pygame.draw.rect(win, (255,0,255), (0,0,450,450))  ## GAME BACKGROUND
+    pygame.draw.rect(win, (255,255,255), (0,0,450,450))  ## GAME BACKGROUND
     ## 4 LINES FOE DIVIDING THE MINE WIN
     pygame.draw.line(win, (0,0,0), (0,150), (450,150), 3)
     pygame.draw.line(win, (0,0,0), (0,300), (450,300), 3)
@@ -29,6 +36,20 @@ def win_dr(win):
     pygame.draw.line(win, (0,0,0), (250,0), (250,450), 1)
     pygame.draw.line(win, (0,0,0), (350,0), (350,450), 1)
     pygame.draw.line(win, (0,0,0), (400,0), (400,450), 1)
+
+    pygame.draw.rect(win, (255,0,0), (pos[0],pos[1],50,50), 2)
+
+    font = pygame.font.Font('freesansbold.ttf', 50)
+
+    for i in range(9):
+        for j in range(9):
+            if arr[i][j] == 0:   ## do not print 0 in game
+                continue
+            text = font.render(str(arr[i][j]), True, (0,0,0))  ## Read the value fron arr
+            win.blit(text,((j*50)+10, i*50))  ## printing the value to the game bord
+
+
+
 
     pygame.display.update()
 
