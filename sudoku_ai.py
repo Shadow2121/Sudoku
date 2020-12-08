60c4cda7c48b92531ed9e1b092e1c565df253eef
Mihir Patel <mihir20011patel@gmail.com>
2020-12-08 23:26:43 +0530

    so many changes
    But so many bugs

--- sudoku_ai.py
+++ sudoku_ai.py
@@ -1,43 +1,122 @@
 import sudoku_che
 import sudoku_drw
 import pygame
+import numpy as np
+
 
 
 def ai(arr, donotchange, win):
-    while True:
-        z = 0
-        for i in range(9):
-            dit = {}
-            for j in range(9):
-                if arr[i, j] == 0:
-                    for t in range(1, 10):
-                        sudoku_che.check(arr, i, j, t, win)
-                        if arr[i,j] != 0:
-                            dit[(i,j)] = t
-                            arr[i][j] = 0
-                            break
-            rev_dict = {}   
-            for key, value in dit.items(): 
-                rev_dict.setdefault(value, set()).add(key) 
-                
-            result = [key for key, values in rev_dict.items() if len(values) > 1] 
-            print(dit, result)
-            
-            for key,value in dit.items():
-                if value not in result:
-                    ii,jj = list(key)[0], list(key)[1]
-                    arr[ii,jj] = value
+    pass
+#     temp_ans = np.zeros((9,9), dtype=int)
+#     temp_ans_2 = np.zeros((9,9), dtype=int)
 
+#     # dnc = donotchange.copy()
+#     while 1:
+#         z = 0
+#         for i in range(9):
+#             for j in range(9):
+#                 if [i,j] in donotchange:
+#                     temp_ans[j,i] = arr[j,i]
 
-            
-        sudoku_drw.drow(win, arr, [0,0], donotchange, 0)
-
-        ## Exit condition
-        for i in range(9):
-            for j in range(9):
-                if arr[i,j] == 0:
-                    z += 1
-        if z == 0:
-            break
-
+#         for i in range(9):
+#             dit = {}
+#             for j in range(9):
+#                 if arr[i, j] == 0:
+#                     for t in range(1, 10):
+#                         sudoku_che.check(arr, i, j, t, win)
+#                         if arr[i,j] != 0:
+#                             dit[(i,j)] = t
+#                             arr[i][j] = 0
+#                             break
+#             # print(dit)
+#             for key, value in dit.items(): 
+#                 temp_ans[list(key)[0], list(key)[1]] = value
         
+#         print(temp_ans)
+
+#         for i in range(9):
+#             for j in range(9):
+#                 # if [i,j] not in dnc:
+#                 temp = temp_ans[i,j]
+#                 aa = sudoku_che.check_for_ai(temp_ans, i, j, temp_ans[i,j])
+#                 if aa == 1:
+#                     temp_ans_2[j,i] = 1
+#                     temp_ans[i,j] = temp
+
+#         print(temp_ans_2)
+
+#         for i in range(9):
+#             for j in range(9):
+#                 if temp_ans_2[j,i] == 1:
+#                     arr[i,j] = temp_ans[i,j]
+#                     # dnc.append([i,j])
+        
+#         # arr = temp_ans
+#         # print(arr)
+
+#         sudoku_drw.drow(win, arr, [0,0], donotchange, 0)
+
+
+#         ## Exit condition
+#         for i in range(9):
+#             for j in range(9):
+#                 if arr[i,j] == 0:
+#                     z += 1
+#         if z == 0:
+#             break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # rev_dict = {}   
+        # for key, value in dit.items(): 
+        #     rev_dict.setdefault(value, set()).add(key) 
+            
+        # result = [key for key, values in rev_dict.items() if len(values) > 1] 
+
+        # for key,value in dit.items():
+        #     if value not in result:
+        #         ii,jj = list(key)[0], list(key)[1]
+        #         arr[ii,jj] = value
--- sudoku_che.py
+++ sudoku_che.py
@@ -39,3 +39,34 @@ def check(arr, i, j, key, win):
                 break
     if caninsert == True:
         arr[i][j] = key
+
+
+def check_for_ai(temp_ans, i, j, key):
+    caninsert = True
+    ## Checking in row if the duplicate is there
+    for q in range(9):
+        if q == i:
+            continue
+        if temp_ans[q][j] == key:
+            caninsert = False  ## If duplicate is there then we can not insert the number
+            break
+    ## Checking in column if the duplicate is there
+    for q in range(9):
+        if q == j:
+            continue
+        if temp_ans[i][q] == key:
+            caninsert = False  ## If duplicate is there then we can not insert the number
+            break
+    ## Checking in 3X3 box if the duplicate is there
+    for q in range((i // 3) * 3, ((i // 3) + 1) * 3):
+        for z in range((j // 3) * 3, ((j // 3) + 1) * 3):
+            if q == i and z == j:
+                continue
+            if temp_ans[q][z] == key:
+                caninsert = False  ## If duplicate is there then we can not insert the number
+                break
+    if caninsert == True:
+        temp_ans[i][j] = key
+        return 1
+    else:
+        return 0
