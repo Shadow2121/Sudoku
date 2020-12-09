7e89f0cf7afe8d00906217c19005b0dad6ff1db1
Mihir Patel <mihir20011patel@gmail.com>
2020-12-09 09:48:32 +0530

    Final AI algorithm
    Made simple algorithm to solve the sudoku completely

--- sudoku_ai.py
+++ sudoku_ai.py
@@ -6,64 +6,71 @@ import numpy as np
 
 
 def ai(arr, donotchange, win):
-    pass
-#     temp_ans = np.zeros((9,9), dtype=int)
-#     temp_ans_2 = np.zeros((9,9), dtype=int)
+    temp_ans = np.zeros((9,9), dtype=int)  ## Making temp array to store temp valus
+    temp_ans_2 = np.zeros((9,9), dtype=int)  ## Making array with 0's and 1's in it to denote if we should enter that number
 
-#     # dnc = donotchange.copy()
-#     while 1:
-#         z = 0
-#         for i in range(9):
-#             for j in range(9):
-#                 if [i,j] in donotchange:
-#                     temp_ans[j,i] = arr[j,i]
+    mod = 0  ## Just variable to change starting value
 
-#         for i in range(9):
-#             dit = {}
-#             for j in range(9):
-#                 if arr[i, j] == 0:
-#                     for t in range(1, 10):
-#                         sudoku_che.check(arr, i, j, t, win)
-#                         if arr[i,j] != 0:
-#                             dit[(i,j)] = t
-#                             arr[i][j] = 0
-#                             break
-#             # print(dit)
-#             for key, value in dit.items(): 
-#                 temp_ans[list(key)[0], list(key)[1]] = value
+    ## Main loop 
+    while 1:
+        z = 0  ## Variable that denotes the number of 0's in arr
         
-#         print(temp_ans)
+        ## Setting values in temp_ans from arr that are in donotchange
+        for i in range(9):
+            for j in range(9):
+                if [i,j] in donotchange:
+                    temp_ans[j,i] = arr[j,i]
 
-#         for i in range(9):
-#             for j in range(9):
-#                 # if [i,j] not in dnc:
-#                 temp = temp_ans[i,j]
-#                 aa = sudoku_che.check_for_ai(temp_ans, i, j, temp_ans[i,j])
-#                 if aa == 1:
-#                     temp_ans_2[j,i] = 1
-#                     temp_ans[i,j] = temp
-
-#         print(temp_ans_2)
-
-#         for i in range(9):
-#             for j in range(9):
-#                 if temp_ans_2[j,i] == 1:
-#                     arr[i,j] = temp_ans[i,j]
-#                     # dnc.append([i,j])
+        ## Making temp prediction and writting it in temp_ans
+        ## And making dict that will help us in placing the values
+        for i in range(9):
+            dit = {}
+            for j in range(9):
+                if arr[i, j] == 0:
+                    for t in range((mod%9)+1, 10):
+                        sudoku_che.check(arr, i, j, t, win)
+                        if arr[i,j] != 0:
+                            dit[(i,j)] = t
+                            arr[i][j] = 0
+                            break
+            # print(dit)
+            ## Putting values in temp_ans using dict
+            for key, value in dit.items(): 
+                temp_ans[list(key)[0], list(key)[1]] = value
         
-#         # arr = temp_ans
-#         # print(arr)
+        # print(temp_ans)
 
-#         sudoku_drw.drow(win, arr, [0,0], donotchange, 0)
+        ## Making temp_ans_2, if the value is unique then place 1 in temp_ans_2 in that palce using temp_ans
+        for i in range(9):
+            for j in range(9):
+                temp = temp_ans[i,j]
+                aa = sudoku_che.check_for_ai(temp_ans, i, j, temp_ans[i,j])
+                if aa == 1:
+                    temp_ans_2[j,i] = 1
+                    temp_ans[i,j] = temp
+
+        # print(temp_ans_2)
+
+        ## Using temp_ans_2 writting ans to arr using temp_ans
+        for i in range(9):
+            for j in range(9):
+                if temp_ans_2[j,i] == 1:
+                    arr[i,j] = temp_ans[i,j]        
+        # print(arr)
+
+        ## Updating display
+        sudoku_drw.drow(win, arr, [0,0], donotchange, 0)
 
 
-#         ## Exit condition
-#         for i in range(9):
-#             for j in range(9):
-#                 if arr[i,j] == 0:
-#                     z += 1
-#         if z == 0:
-#             break
+        ## Exit condition
+        for i in range(9):
+            for j in range(9):
+                if arr[i,j] == 0:
+                    z += 1
+        if z == 0:
+            break
+
+        mod += 1
 
 
 
